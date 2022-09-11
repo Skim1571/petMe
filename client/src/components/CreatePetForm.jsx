cb14bc4407c37b3ca0077a54fc01db6cf49f8ae3
Skim1571 <skim1571@gmail.com>
2022-09-11 14:37:42 -0500

    cleaned up some code createpetform

--- client/src/components/CreatePetForm.jsx
+++ client/src/components/CreatePetForm.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react"
-import { BASE_URL } from "../globals"
-import { tokenAccessCreator, getCookie } from "../services/Auth"
-import axios from "axios"
 import jwt from "jwt-decode"
 import { Client } from "../services/api"
 
-const auth = require('../services/Auth')
 
 export const CreatePetForm = ({ authToken, user, speciesList }) => {
   const [newPetDetails, setNewPetDetails] = useState({})
@@ -23,36 +19,13 @@ export const CreatePetForm = ({ authToken, user, speciesList }) => {
     event.preventDefault()
     let userInfo = jwt(authToken.access)
     let speciesData = speciesList.filter(species => species.id === parseInt(newPetDetails.speciesChoice))
-    // let data = {
-    //   "username": userInfo.username,
-    //   "password": "abcd",
-    //   "id": userInfo.user_id,
-    //   "pets": {
-    //     "name": newPetDetails.name,
-    //     "species": speciesData[0],
-    //     "image_url": speciesData[0].image_url
-    //   }
-    // }
-    // console.log('data', data)
-    // let res = await Client.put(`/users/${userInfo.user_id}`, data)
-    // console.log('post', res)
-
     let petData = {
       "user": userInfo.user_id,
       "name": newPetDetails.name,
       "species": speciesData[0],
       "image_url": speciesData[0].image_url
     }
-    console.log('data', petData)
     let res = await Client.post(`/pets/`, petData)
-    console.log('post', res)
-    // let pet_id = res.data.id
-    // let putData = {
-    //   "pet"
-    // }
-
-    // let putres = await Client.put(`/users/${userInfo.user_id}`,)
-    // console.log('putres', putres)
   }
 
   switch (parseInt(newPetDetails.speciesChoice)) {
